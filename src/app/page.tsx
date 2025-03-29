3613cdeaa5307d2c115a6f6d499e5c45fa9690ce
ThuverX <exozgames@gmail.com>
2025-03-29 16:12:58 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,108 @@
-import Image from "next/image";
+"use client";
+
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Download, Upload, ArrowRight} from "lucide-react"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {FileEntry, SaveConverter} from "@/system/SaveConverter";
+import {useState} from "react";
+import { saveAs } from 'file-saver';
+import {downloadZip} from "client-zip";
+
+const converter = new SaveConverter();
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const [ files, setFiles ] = useState<FileEntry[]>([]);
+
+    const handleFile = async (file: File) => {
+        setFiles([])
+
+        await converter.read(file)
+
+        setFiles(converter.files)
+    }
+
+    const handleFolder = async (files: File[]) => {
+        setFiles([])
+
+        await converter.fromFolder(files)
+
+        setFiles(converter.files)
+    }
+
+    const saveFile = async () => {
+        if(files.length <= 0) return
+
+        saveAs(new Blob([converter.write()]), converter.saveName + ".sav")
+    }
+
+    const saveZip = async () => {
+        if(files.length <= 0) return
+
+        const fileList: File[] = []
+
+        for (const file of converter.files) {
+            fileList.push(new File([file.DataUncompressed], file.Name))
+        }
+
+        const zip = downloadZip(fileList, {})
+
+        saveAs(await zip.blob(), converter.saveName + ".zip")
+    }
+
+    return (
+        <div className="flex items-center justify-center grow min-h-screen ">
+            <div className="flex flex-col">
+                <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+                    MySims Save Converter
+                </h3>
+                <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                    <li>Drop in your <code>.sav</code> file <i>(From MySims Cozy Bundle)</i></li>
+                    <li>or your <code>SaveGame</code> folder <i>(From Release or Taco Bell)</i></li>
+                </ul>
+                <Tabs defaultValue="old->new">
+                    <TabsList className="w-full">
+                        <TabsTrigger className="cursor-pointer" value="old->new">Old <ArrowRight/> New</TabsTrigger>
+                        <TabsTrigger className="cursor-pointer" value="new->old">New <ArrowRight/> Old</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="old->new" >
+                        <div className="container flex gap-8 mt-8">
+                            <Upload className="m-auto"/>
+                            <Input
+                                type="file"
+                                webkitdirectory="true"
+                                className="cursor-pointer"
+                                onChange={(e) => handleFolder([...e.target.files!]) }
+                            />
+                            <Button disabled={files.length <= 0} className="cursor-pointer" onClick={saveFile}>
+                                <Download className="m-auto mr-2"/>
+                                Download to .sav
+                            </Button>
+                        </div>
+                    </TabsContent>
+                    <TabsContent value="new->old">
+                        <div className="container flex gap-8 mt-8">
+                            <Upload className="m-auto"/>
+                            <Input
+                                type="file"
+                                accept=".sav"
+                                className="cursor-pointer"
+                                onChange={(e) => handleFile(e.target.files![0]) }
+                            />
+                            <Button disabled={files.length <= 0} className="cursor-pointer" onClick={saveZip}>
+                                <Download className="m-auto mr-2"/>
+                                Download to .zip
+                            </Button>
+                        </div>
+                    </TabsContent>
+                </Tabs>
+                <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                    <li>Your Cozy Bundle save games can be found in <code>%LOCALAPPDATA%/Electronic Arts/MySims</code></li>
+                    <li>And your old save games can be found in <code>%USERPROFILE%/Documents/Electronic Arts/MySims</code></li>
+                </ul>
+                <h2>Check out the source code for this website on <a href="" className="text-blue-500">Github</a></h2>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
---
+++ src/system/SaveConverter.ts
@@ -0,0 +1,122 @@
+﻿import { Buffer } from "node:buffer";
+import zlib from 'node:zlib';
+
+export class FileEntry {
+
+    public NameLength: number = 0
+    public Name: string = ""
+    public Size: number = 0
+    public UncompressedSize: number = 0
+    public DataCompressed: Uint8Array  = new Uint8Array ()
+    public DataUncompressed: Uint8Array  = new Uint8Array ()
+    public Checksum: number = 0
+
+    public read(view: DataView, offset: number): number {
+        this.NameLength = view.getUint32(offset, true); offset+=4
+        this.Name = ""
+
+        for(let i = 0; i < this.NameLength; i++) {
+            const byte = view.getUint8(offset); offset+=1
+            this.Name += String.fromCharCode(byte)
+        }
+
+        this.Size = view.getUint32(offset, true); offset+=4
+        offset+=4
+        this.UncompressedSize = view.getUint32(offset, true); offset+=4
+        offset+=4
+        this.DataCompressed = new Uint8Array(view.buffer.slice(offset, offset + this.Size)); offset+=this.Size
+        this.DataUncompressed = zlib.inflateSync(Buffer.from(this.DataCompressed))
+        this.Checksum = view.getUint8(offset); offset+=1
+
+        return offset
+    }
+
+    public async fromFolder(file: File) {
+        this.Name = file.webkitRelativePath
+        this.Size = file.size
+        this.DataUncompressed = new Uint8Array(await file.arrayBuffer())
+        this.DataCompressed = zlib.deflateRawSync(Buffer.from(this.DataUncompressed), {
+            level: -1
+        })
+        this.Checksum = 1
+    }
+
+    public GetText() {
+        return new TextDecoder("utf-8").decode(this.DataUncompressed)
+    }
+
+    public write(view: DataView, offset: number): number {
+        view.setUint32(offset, this.Name.length, true); offset+=4
+        for (let i = 0; i < this.Name.length; i++) {
+            const byte = this.Name.charCodeAt(i)
+            view.setInt8(offset, byte); offset+=1
+        }
+
+        view.setUint32(offset, this.DataCompressed.length, true); offset+=4
+        view.setUint32(offset, 0, true); offset+=4
+        view.setUint32(offset, this.Size, true); offset+=4
+        view.setUint32(offset, 0, true); offset+=4
+
+        for(let i = 0; i < this.DataCompressed.length; i++) {
+            view.setUint8(offset, this.DataCompressed[i]); offset+=1
+        }
+        view.setUint8(offset, 1); offset+=1
+
+        return offset
+    }
+}
+
+export class SaveConverter {
+
+    public files: Array<FileEntry> = []
+    public fileCount: number = 0
+    public saveName: string = ""
+
+    public async read(file: File): Promise<number> {
+        this.saveName = file.name.split(".")[0]
+        this.files = []
+        const view = new DataView(await file.arrayBuffer())
+        let offset = 0
+        this.fileCount = view.getUint32(offset, true); offset+=4
+
+        for(let i = 0; i < this.fileCount; i++) {
+            const entry = new FileEntry()
+
+            offset = entry.read(view, offset)
+
+            this.files.push(entry)
+        }
+
+        return offset
+    }
+
+    public async fromFolder(files: File[]): Promise<number> {
+        this.files = []
+
+        this.saveName = files[0].webkitRelativePath.split(/\\|\//)[0]
+
+        console.log(files[0].webkitRelativePath)
+
+        for(const file of files) {
+            const entry = new FileEntry()
+
+            await entry.fromFolder(file)
+
+            this.files.push(entry)
+        }
+
+        return 0
+    }
+
+    public write(): ArrayBuffer {
+        const view = new DataView(new ArrayBuffer(2_000_000_000))
+        let offset = 0
+        view.setUint32(offset, this.files.length, true); offset+=4
+
+        for(const file of this.files) {
+            offset = file.write(view, offset)
+        }
+
+        return view.buffer.slice(0, offset)
+    }
+}
